e5500dd5b50fa3bc68f7d42915e3eb9344dd59c2
Jean Perbet <jeanperbet@icloud.com>
2023-12-22 12:50:20 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# Import the Canvas class
+from canvasapi import Canvas
+
+# KTH Canvas API URL
+API_URL = "https://canvas.kth.se"
+
+# KTH CANVAS API KEY
+API_KEY = "8779~j14i49cxqgeYv9W1m4FKqwvxS0pGsJUvPdiqHkeACu2bVRjg1WJzSlfqOUxhaU6w"
+
+# Initialize a new Canvas object
+canvas = Canvas(API_URL, API_KEY)
+
+if __name__ == "__main__":
+   pass
+
+
